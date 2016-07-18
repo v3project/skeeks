2e294b749ed52396aeadd9a1f70f7cfc4db960e5
Саша <semenov@skeeks.com>
2016-07-19 01:17:01 +0300

    Yupe import

--- config/agents.php
+++ config/agents.php
@@ -1,11 +1,11 @@
 <?php
 return
 [
-    /*'v3toys/agents/products-update' =>
+    'v3toys/agents/products-update' =>
     [
         'description'       => 'Обновление цен и налчия товаров',
-        'agent_interval'    => 3600*1, //раз в час
-        'next_exec_at'      => \Yii::$app->formatter->asTimestamp(time()) + 3600*3,
+        'agent_interval'    => 3600*0.5, //раз в 30 минут
+        'next_exec_at'      => \Yii::$app->formatter->asTimestamp(time()) + 3600*0.5,
         'is_period'         => 'N'
-    ]*/
+    ]
 ];
--- console/controllers/AgentsController.php
+++ console/controllers/AgentsController.php
@@ -9,6 +9,8 @@ namespace v3toys\skeeks\console\controllers;
 
 use skeeks\cms\shop\models\ShopCmsContentElement;
 use yii\console\Controller;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Console;
 
 /**
  * Агенты v3toys
@@ -28,13 +30,55 @@ class AgentsController extends Controller
         $contentIds = (array) \Yii::$app->v3toysSettings->content_ids;
         if (!$contentIds)
         {
-            $this->stdout("Import products from yupe: {$total}\n", Console::BOLD);
+            $this->stdout("Не настроен v3toys комонент: {$total}\n", Console::FG_RED);
+            return;
         }
 
-        //ShopCmsContentElement::find()->where(['content_id' => ])
+        $elements = ShopCmsContentElement::find()->where(['content_id' => $contentIds])->all();
+        if ($elements)
+        {
+            $total = count($elements);
+            $this->stdout("Всего товаров: {$total}\n", Console::BOLD);
 
+            /**
+             * @var $element ShopCmsContentElement
+             */
+            foreach ($elements as $element)
+            {
+                $this->stdout("\t{$element->id}: {$element->name}\n");
+                $v3id = $element->relatedPropertiesModel->getAttribute(\Yii::$app->v3toysSettings->v3toysIdPropertyName);
+                if ($v3id)
+                {
+                    $response = \Yii::$app->v3toysApi->getProductsDataByIds(['products_ids' => $v3id]);
+                    if ($response->isOk)
+                    {
+                        if ($response->data)
+                        {
+                            $data = $response->data[0];
 
+                            $element->shopProduct->purchasing_price = ArrayHelper::getValue($data, 'buy_price');
+                            $element->shopProduct->purchasing_currency = "RUB";
 
+                            $element->shopProduct->baseProductPriceValue = ArrayHelper::getValue($data, 'price');
+                            $element->shopProduct->baseProductPriceCurrency = "RUB";
+                            $element->shopProduct->quantity = ArrayHelper::getValue($data, 'quantity');
+
+                            if ($element->shopProduct->save())
+                            {
+                                $this->stdout("\tЦена={$element->shopProduct->baseProductPriceValue}; Количество={$element->shopProduct->quantity}\n", Console::FG_GREEN);
+                            } else
+                            {
+                                $this->stdout("\tЦена и количество не обновлено\n", Console::FG_RED);
+                            }
+                        }
+                    }
+                } else
+                {
+                    $this->stdout("\t{$element->id}: {$element->name}\n", Console::FG_RED);
+                    continue;
+                }
+            }
+        }
     }
 
     /**
