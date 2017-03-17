9ac6d1545b49d6d809db74311ee8419bf83d6d1b
Semenov <semenov@skeeks.com>
2017-03-17 21:28:39 +0300

    dev

--- src/console/controllers/ProductsController.php
+++ src/console/controllers/ProductsController.php
@@ -7,6 +7,7 @@
  */
 namespace v3toys\skeeks\console\controllers;
 
+use skeeks\cms\components\Cms;
 use skeeks\cms\relatedProperties\propertyTypes\PropertyTypeText;
 use skeeks\cms\shop\models\ShopCmsContentElement;
 use skeeks\cms\shop\models\ShopPersonTypeProperty;
@@ -29,6 +30,8 @@ use yii\httpclient\Client;
  */
 class ProductsController extends Controller
 {
+    protected $affData = [];
+
     /**
      * Обновление цен и налчия товаров
      */
@@ -38,12 +41,6 @@ class ProductsController extends Controller
         set_time_limit(0);
 
 
-        $query = (new \yii\db\Query())
-                    ->from('apiv5.affproduct');
-
-        //$prices = $query->all(\Yii::$app->dbV3project);
-
-        //Проверка настройки компонента
         $contentIds = (array) \Yii::$app->v3toysSettings->content_ids;
         if (!$contentIds)
         {
@@ -51,45 +48,101 @@ class ProductsController extends Controller
             return;
         }
 
-        $count = $query->count(\Yii::$app->dbV3project);
-        $this->stdout("Всего товаров базе 3project для вашего аффилиата: {$count}\n", Console::BOLD);
+        $contentId = $contentIds[0];
+        
+        $queryAff = (new \yii\db\Query())
+                    ->from('apiv5.affiliate')
+        ;
 
+        $this->affData = $queryAff->one(\Yii::$app->dbV3project);
 
-        foreach ($query->each(100, \Yii::$app->dbV3project) as $row)
+        if (!$this->affData)
         {
-            //Данные по каждому товару аффилиата
-            $v3id = ArrayHelper::getValue($row, 'product_id');
+            $message = 'Нет данных по аффилиату';
+            $this->stdout("$message\n", Console::FG_RED);
+            return;
+        }
+        
+        $query = (new \yii\db\Query())
+                    ->from('apiv5.affproduct')
+                    ->where(["!=", 'title', ""])
+                    ->andWhere(["!=", 'description', ""])
+                    ->andWhere(["!=", 'main_image_path', ""])
+                    ->andWhere(["!=", 'second_image_paths', ""])
+        ;
+
+        $count = $query->count("*", \Yii::$app->dbV3project);
+        $this->stdout("Всего товаров: {$count}\n", Console::BOLD);
+        /**
+         * Array
+(
+    [id] => 55130
+    [created_at] => 2016-08-22 17:03:49
+    [updated_at] => 2016-09-27 00:10:30
+    [affiliate_id] => 2
+    [fr_aff_key] => 252821
+    [product_id] => 324164
+    [title] => Пластиковый конструктор "Мусороуборочная машина", 102 детали
+    [alt_title] =>
+    [description] => <p>тестовый текст от Коваленко</p>
+    [meta_title] => МЕТА заголовок товара
+    [meta_keywords] =>
+    [meta_description] => МЕТА описание
+    [main_image_path] => 57e18d7687fc5.jpg
+    [second_image_paths] =>
+)
+
+         */
+        foreach ($query->orderBy(['id' => SORT_DESC])->each(100, \Yii::$app->dbV3project) as $row)
+        {
+            //print_r($row);die;
+
+            $v3id   = ArrayHelper::getValue($row, 'product_id');
+            $id     = ArrayHelper::getValue($row, 'id');
             if (!$v3id)
             {
                 $this->stdout("\t\t Not found v3id\n", Console::FG_RED);
                 continue;
             }
 
-            $this->stdout("\t\t v3p_product_id: {$v3id}\n");
+            $this->stdout("\t\t V3productId: {$v3id}\n");
+            $this->stdout("\t\t Affiliat Id: {$id}\n");
 
-            if (V3toysProductContentElement::find()->joinWith('v3toysProductProperty as p')->andWhere(['p.v3toys_id' => $v3id])->exists())
+            if ($element = V3toysProductContentElement::find()->joinWith('v3toysProductProperty as p')->andWhere(['p.v3toys_id' => $v3id])->one())
             {
                 $this->stdout("\t\t exist: {$v3id}\n", Console::FG_YELLOW);
+
+                $element->meta_keywords = (string) ArrayHelper::getValue($row, 'meta_title');
+                $element->meta_title = (string) ArrayHelper::getValue($row, 'meta_title');
+                $element->meta_description = (string) ArrayHelper::getValue($row, 'meta_description');
+                $element->name = (string) ArrayHelper::getValue($row, 'title');
+                $element->description_full = (string) ArrayHelper::getValue($row, 'description');
+                $element->save();
+                
+                $this->_imageUpdate($element, $row);
+                $this->_imagesUpdate($element, $row);
+                
                 continue;
             }
+
 
             $property                       = new V3toysProductProperty();
             $property->consoleController    = $this;
             $property->v3toys_id            = ArrayHelper::getValue($row, 'product_id');
 
+
             $element = new V3toysProductContentElement();
-            $element->name          = $title;
-            $element->content_id    = 2;
-            $element->meta_keywords = ArrayHelper::getValue($row, 'keywords');
+            $element->content_id    = $contentId;
+            $element->meta_keywords = (string) ArrayHelper::getValue($row, 'meta_title');
+            $element->meta_title = (string) ArrayHelper::getValue($row, 'meta_title');
+            $element->meta_description = (string) ArrayHelper::getValue($row, 'meta_description');
+            $element->name = (string) ArrayHelper::getValue($row, 'title');
+            $element->description_full = (string) ArrayHelper::getValue($row, 'description');
 
             if ($element->save())
             {
                 $this->stdout("\t\t Element added\n", Console::FG_GREEN);
-
-
                 $property->id = $element->id;
-
-                $property->sku = ArrayHelper::getValue($row, 'sku');
 
                 if ($property->save())
                 {
@@ -97,25 +150,132 @@ class ProductsController extends Controller
                 } else
                 {
                     $this->stdout("\t\t Property not added\n", Console::FG_RED);
-                    print_r($property->errors);
                     if ($element->delete())
                     {
                         $this->stdout("\t\t Element deleted\n");
                     }
-                    die;
                 }
 
             } else
             {
                 $this->stdout("\t\t Element not added\n", Console::FG_RED);
                 print_r($element->errors);
-                die;
             }
 
 
 
-        }
 
+
+            
+            $this->_imageUpdate($element, $row);
+            $this->_imagesUpdate($element, $row);
+            
+            die;
+        }
+    }
+    
+    
+    protected function _imagesUpdate($element, $row)
+    {
+        $imagePatternPath = ArrayHelper::getValue($this->affData, 'affproduct_image_url_pattern');
+
+        //Работа с главным изображением
+        $second_image_paths = ArrayHelper::getValue($row, 'second_image_paths');
+
+        if ($second_image_paths && !$element->images)
+        {
+            $second_image_paths = Json::decode($second_image_paths);
+            $this->stdout("\t\t Загрузка картинок\n", Console::FG_GREEN);
+
+            foreach ((array) $second_image_paths as $imageName)
+            {
+                $realImagePath = str_replace('{image_path}', $imageName, $imagePatternPath);
+                
+                try
+                {
+                    $this->stdout("\t\t\t Загрузка картинки: {$realImagePath}\n", Console::FG_GREEN);
+
+                    $file = \Yii::$app->storage->upload($realImagePath, [
+                        'name' => $element->name
+                    ]);
+
+                    $element->link('images', $file);
+                    $file->original_name = $imageName;
+                    $file->save();
+
+                    $this->stdout("\t\t\t Загружено\n", Console::FG_GREEN);
+                } catch (\Exception $e)
+                {
+                    $message = $e->getMessage();
+                    $this->stdout("\t\t\t {$message}\n", Console::FG_RED);
+                }
+            }
+        }
+        
+        return $this;
+    }
+    
+    protected function _imageUpdate($element, $row)
+    {
+        $imagePatternPath = ArrayHelper::getValue($this->affData, 'affproduct_image_url_pattern');
+
+        //Работа с главным изображением
+        $mainImage = ArrayHelper::getValue($row, 'main_image_path');
+
+        if ($mainImage)
+        {
+            $realImagePath = str_replace('{image_path}', $mainImage, $imagePatternPath);
+
+            //Если нет картинки
+            if (!$element->image)
+            {
+                try
+                {
+                    $this->stdout("\t\t Загрузка картинки: {$realImagePath}\n");
+                    $file = \Yii::$app->storage->upload($realImagePath, [
+                        'name' => $element->name
+                    ]);
+
+                    $element->link('image', $file);
+                    $file->original_name = $mainImage;
+                    $file->save();
+
+                    $this->stdout("\t\t\t Загружено\n", Console::FG_GREEN);
+                } catch (\Exception $e)
+                {
+                    $message = $e->getMessage();
+                    $this->stdout("\t\t\t {$message}\n", Console::FG_RED);
+                }
+
+            } else
+            {
+
+                if ($element->image->original_name != $mainImage)
+                {
+                    try
+                    {
+                        $this->stdout("\t\t Главная картинка изменилась\n", Console::FG_YELLOW);
+                        $this->stdout("\t\t Загрузка картинки\n");
+
+                        $file = \Yii::$app->storage->upload($realImagePath, [
+                            'name' => $element->name
+                        ]);
+
+                        $element->link('image', $file);
+                        $file->original_name = $mainImage;
+                        $file->save();
+
+                        $this->stdout("\t\t\t Загружено\n", Console::FG_GREEN);
+                    } catch (\Exception $e)
+                    {
+                        $message = $e->getMessage();
+                        $this->stdout("\t\t\t {$message}\n", Console::FG_RED);
+                    }
+                }
+            }
+        }
+        
+        return $this;
     }
 
 }
