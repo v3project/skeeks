d477ff21bdd254bd545a09fb5a740f1af3fd5026
Саша <semenov@skeeks.com>
2017-03-23 23:01:53 +0300

    Убран лишний агент

--- src/config/agents.php
+++ src/config/agents.php
@@ -9,13 +9,13 @@ return
         'is_period'         => 'N'
     ],
 
-    'v3toys/agents/products-update' =>
+    /*'v3toys/agents/products-update' =>
     [
         'description'       => 'Обновление цен и наличия товаров',
         'agent_interval'    => 3600*0.5, //раз в 30 минут
         'next_exec_at'      => \Yii::$app->formatter->asTimestamp(time()) + 3600*0.5,
         'is_period'         => 'N'
-    ],
+    ],*/
 
     'v3toys/agents/submit-new-orders' =>
     [
