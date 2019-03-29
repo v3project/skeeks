9ba6d61aaf523b9869293bb90fcfabdb736a8e68
semenov <semenov@skeeks.com>
2019-03-29 21:08:53 +0300

    dev

--- src/controllers/AdminV3ShopCmsContentElementController.php
+++ src/controllers/AdminV3ShopCmsContentElementController.php
@@ -9,25 +9,13 @@
 namespace v3toys\skeeks\controllers;
 
 use skeeks\cms\backend\actions\BackendModelAction;
-use skeeks\cms\backend\actions\BackendModelUpdateAction;
 use skeeks\cms\components\marketplace\models\PackageModel;
-use skeeks\cms\helpers\UrlHelper;
 use skeeks\cms\models\Comment;
-use skeeks\cms\modules\admin\actions\AdminAction;
-use skeeks\cms\modules\admin\controllers\AdminController;
-use skeeks\cms\modules\admin\controllers\AdminModelEditorController;
-use skeeks\cms\modules\admin\traits\AdminModelEditorStandartControllerTrait;
 use skeeks\cms\shop\controllers\AdminCmsContentElementController;
 use skeeks\sx\helpers\ResponseHelper;
-use v3toys\skeeks\models\V3toysOrder;
-use v3toys\skeeks\models\V3toysOrderStatus;
 use v3toys\skeeks\models\V3toysProductContentElement;
 use v3toys\skeeks\models\V3toysProductProperty;
 use v3toys\skeeks\models\V3toysShippingCity;
-use Yii;
-use skeeks\cms\models\User;
-use skeeks\cms\models\searchs\User as UserSearch;
-use yii\helpers\ArrayHelper;
 
 /**
  * Class AdminV3ShopCmsContentElementController
@@ -35,22 +23,20 @@ use yii\helpers\ArrayHelper;
  */
 class AdminV3ShopCmsContentElementController extends AdminCmsContentElementController
 {
-    protected $_modelClassName = V3toysProductContentElement::class;
+    public $modelClassName = V3toysProductContentElement::class;
 
     public function actions()
     {
         $actions = parent::actions();
 
-        if ($this->content && \Yii::$app->v3toysSettings->content_ids && in_array($this->content->id,
-                (array)\Yii::$app->v3toysSettings->content_ids)) {
-            $actions['v3project'] =
-                [
-                    'class' => BackendModelAction::class,
-                    'name' => 'Сязь с V3project',
-                    "icon" => "fa fa-child",
-                    "priority" => 500,
-                    "callback" => [$this, 'actionV3project'],
-                ];
+        if ($this->content && \Yii::$app->v3toysSettings->content_ids && in_array($this->content->id, (array)\Yii::$app->v3toysSettings->content_ids)) {
+            $actions['v3project'] = [
+                'class'    => BackendModelAction::class,
+                'name'     => 'Сязь с V3project',
+                "icon"     => "fa fa-child",
+                "priority" => 500,
+                "callback" => [$this, 'actionV3project'],
+            ];
         }
 
         return $actions;
@@ -90,7 +76,7 @@ class AdminV3ShopCmsContentElementController extends AdminCmsContentElementContr
         }
 
         return $this->render('@v3toys/skeeks/views/admin-cms-content-element/v3project', [
-            'property' => $property
+            'property' => $property,
         ]);
     }
 
